f86d4d82c17bff903fb069bdf2891710e62e4511
Marlan McInnes-Taylor <marlanmct@gmail.com>
2019-02-03 22:22:24 -0500

    definition of CrazyRandomSword class

---
+++ CrazyRandomSword.cpp
@@ -0,0 +1,21 @@
+/*
+ * File:   SimpleHammer.cpp
+ * Author: Marlan <mm05f@my.fsu.edu>
+ *
+ * Created on February 3, 2019, 4:15 PM
+ */
+
+#include "CrazyRandomSword.h"
+
+double CrazyRandomSword::hit(double armor) {
+    double damage = 0;
+    int third = ;
+    if(armor <= 6)
+      damage = hitpoints - (armor - 2);
+    else
+      damage = hitPoints - (armor - (rand()%((int)ceil(armor * .3) - 2) + 2));
+    if (damage < 0) {
+        return 0;
+    }
+    return damage;
+}
